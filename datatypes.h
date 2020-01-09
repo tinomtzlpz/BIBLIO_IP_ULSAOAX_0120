cdae5d2b4c5cdcb3eea11caa9e2333bf94b86744
tinomtzlpz <43123591+tinomtzlpz@users.noreply.github.com>
2020-01-09 13:53:53 -0600

    Add files via upload
    Archivos iniciales para el proyecto de biblioteca

---
+++ datatypes.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   datatypes.h
+ * Author: tino
+ *
+ * Created on 9 de enero de 2020, 01:04 PM
+ */
+
+#ifndef DATATYPES_H
+#define	DATATYPES_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    typedef enum
+    {
+        Contaduria, Negocios, Arquitectura, Enfermeria, Fisioterapia, Psicologia,
+                Nutricion, Deporte, Artes, Comunicacion, Derecho, Educacion,
+                Gastronomia, Turismo, Lenguas, Electronica, Sistemas, Industrial,
+                Civil, Ambiental
+    } carreras;
+
+    typedef struct
+    {
+        unsigned int matricula;
+        char nombre[50];
+        carreras carrera;
+    } usuario;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DATATYPES_H */
+
---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: tino
+ *
+ * Created on 9 de enero de 2020, 01:03 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "datatypes.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("El número asociada a la carrera Turismo es: %d", turismo);
+    return (EXIT_SUCCESS);
+}
+
